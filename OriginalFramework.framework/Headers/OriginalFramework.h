233985f468d6134010c5acba29edf237571d22ae
ngo275 <ngo38114@gmail.com>
2019-10-25 17:41:29 +0900

    Added Framework

---
+++ OriginalFramework.framework/Headers/OriginalFramework.h
@@ -0,0 +1,19 @@
+//
+//  OriginalFramework.h
+//  OriginalFramework
+//
+//  Created by ShuichiNagao on 2019/10/23.
+//  Copyright © 2019 Shuichi Nagao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OriginalFramework.
+FOUNDATION_EXPORT double OriginalFrameworkVersionNumber;
+
+//! Project version string for OriginalFramework.
+FOUNDATION_EXPORT const unsigned char OriginalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OriginalFramework/PublicHeader.h>
+
+
